88938cedb28d6ca5b64bf3a4cb61165bfeb04158
widrns15 <widrns15@naver.com>
2022-12-27 23:48:23 +0900

    feat: 네비게이션 컴포넌트 추가

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
+
 import './App.css';
+import Navigation from './components/nevigation';
 
 function App() {
   return (
     <div className="App">
+      <Navigation />
     프로젝트 걸음마
   
     </div>

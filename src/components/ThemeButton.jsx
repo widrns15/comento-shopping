59f46545ad040bd0d14e0b70fa5b7867c91fb757
widrns15 <widrns15@naver.com>
2023-01-03 04:23:22 +0900

    feat: Home 페이지 디자인 적용

--- src/components/ThemeButton.jsx
+++ src/components/ThemeButton.jsx
@@ -1,9 +1,18 @@
+import styled from "styled-components";
+
 const ThemeButton = ({ ThemeName }) => {
-  return (
-    <div>
-      <div>{ThemeName}</div>
-    </div>
-  );
+  return <ThemeButtonStyled>{ThemeName}</ThemeButtonStyled>;
 };
+
+const ThemeButtonStyled = styled.div`
+  background: rgba(0, 0, 0, 0.5);
+  width: fit-content;
+  border-radius: 10px;
+  font-weight: 700;
+  font-size: 20px;
+  line-height: 26px;
+  padding: 24px 16px;
+  color: white;
+`;
 
 export default ThemeButton;

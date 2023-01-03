58a9df93b25968b237d0ec9af5e38694998e8064
widrns15 <widrns15@naver.com>
2023-01-03 15:59:11 +0900

    Revert "feat: Home 페이지 디자인 적용"
    This reverts commit c813273475a021b57973f9d09b9fda22c1630ffb.

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,34 +1,11 @@
-import styled from "styled-components";
-
 const ProductCard = ({ Name, Description, Thumnail }) => {
   return (
-    <ProductCardStyled>
-      <img width={341} src={Thumnail} alt={Name} />
-      <NameStyled>{Name}</NameStyled>
-      <DescriptionStyled>{Description}</DescriptionStyled>
-    </ProductCardStyled>
+    <div>
+      <img width={200} src={Thumnail} alt={Name} />
+      <div>{Name}</div>
+      <div>{Description}</div>
+    </div>
   );
 };
-const ProductCardStyled = styled.div`
-  padding: 20px 24px;
-`;
-
-const NameStyled = styled.div`
-  width: fit-content;
-  font-weight: 700;
-  font-size: 20px;
-  line-height: 26px;
-  padding: 12px 0px;
-  color: black;
-`;
-
-const DescriptionStyled = styled.div`
-  width: fit-content;
-  font-weight: 400;
-  font-size: 16px;
-  line-height: 21px;
-  letter-spacing: -0.01em;
-  color: black;
-`;
 
 export default ProductCard;

0404d022416f126b520e131fc85ef2d0ad6b9907
widrns15 <widrns15@naver.com>
2023-01-03 15:35:33 +0900

    feat: Home 페이지 디자인 적용

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,11 +1,34 @@
+import styled from "styled-components";
+
 const ProductCard = ({ Name, Description, Thumnail }) => {
   return (
-    <div>
-      <img width={200} src={Thumnail} alt={Name} />
-      <div>{Name}</div>
-      <div>{Description}</div>
-    </div>
+    <ProductCardStyled>
+      <img width={341} src={Thumnail} alt={Name} />
+      <NameStyled>{Name}</NameStyled>
+      <DescriptionStyled>{Description}</DescriptionStyled>
+    </ProductCardStyled>
   );
 };
+const ProductCardStyled = styled.div`
+  padding: 20px 24px;
+`;
+
+const NameStyled = styled.div`
+  width: fit-content;
+  font-weight: 700;
+  font-size: 20px;
+  line-height: 26px;
+  padding: 12px 0px;
+  color: black;
+`;
+
+const DescriptionStyled = styled.div`
+  width: fit-content;
+  font-weight: 400;
+  font-size: 16px;
+  line-height: 21px;
+  letter-spacing: -0.01em;
+  color: black;
+`;
 
 export default ProductCard;

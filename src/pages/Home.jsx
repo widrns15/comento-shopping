96c73cae741467a9730e117e65ebf920d768895e
widrns15 <widrns15@naver.com>
2023-01-05 23:35:28 +0900

    fix: Home 페이지 레이아웃, Navigation 파일명 수정

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,4 @@
-import Nevigation from "../components/nevigation";
+import Navigation from "../components/Navigation";
 import ThemeButton from "../components/ThemeButton";
 import ProductCard from "../components/ProductCard";
 import styled from "styled-components";
@@ -7,8 +7,10 @@ const Home = () => {
   return (
     <MainSection>
       <div>
-        <Nevigation />
-        <Topbar />
+        <div>
+          <Navigation />
+          <Topbar />
+        </div>
         <ThemeSection>
           <ThemeButton ThemeName={"#겨울 방한템"} />
           <ThemeButton ThemeName={"#따순 머그컵"} />
@@ -16,29 +18,37 @@ const Home = () => {
         <GrayLine />
       </div>
 
-      <div>
-        <MiddleBar />
-
+      <ProductCards>
         <ProductCard
           Name="비숑 블랙 머그잔"
-          Description="쌀쌀한 날씨에 따뜻한 우유, 커피 한잔하기 좋은 블랙 & 화이트 비숑 머그잔입니다."
+          Description={
+            "쌀쌀한 날씨에 따뜻한 우유, 커피 한잔하기 좋은 \n 블랙 & 화이트 비숑 머그잔입니다."
+          }
           Thumnail="https://raw.githubusercontent.com/congchu/coment-shop-server/master/assets/images/product1.jpg"
         />
         <ProductCard
           Name="가열 보온 티코스터 온열 원터치 컵 받침대"
-          Description="언제나 따뜻하게 최대 12시간 동안 최대 60도의 온도로 따듯한 차를 즐길 수 있습니다."
+          Description={
+            "언제나 따뜻하게 최대 12시간 동안 \n 최대 60도의 온도로 따듯한 차를 즐길 수 있습니다."
+          }
           Thumnail="https://raw.githubusercontent.com/congchu/coment-shop-server/master/assets/images/product3.jpg"
         />
         <ProductCard
           Name="벨루즈까사 솜사탕 파스텔 머그 4종 세트"
-          Description="솜사탕처럼 부드러운 쉐입에 스트라이프 조각이 더해져 심플하면서도 세련된 파스텔 컬러의 머그"
+          Description={
+            "솜사탕처럼 부드러운 쉐입에 스트라이프 조각이 \n 더해져 심플하면서도 세련된 파스텔 컬러의 머그"
+          }
           Thumnail="https://raw.githubusercontent.com/congchu/coment-shop-server/master/assets/images/product2.jpg"
         />
-        <BottomBar />
-      </div>
+      </ProductCards>
     </MainSection>
   );
 };
+
+const ProductCards = styled.div`
+  padding-top: 20px;
+  padding-bottom: 82px;
+`;
 
 const MainSection = styled.div`
   position: relative;
@@ -61,18 +71,6 @@ const GrayLine = styled.div`
   height: 8px;
   width: 100%;
   background: #eeeeee;
-`;
-
-const MiddleBar = styled.div`
-  height: 20px;
-  width: 100%;
-  color: white;
-`;
-
-const BottomBar = styled.div`
-  height: 82px;
-  width: 100%;
-  color: white;
 `;
 
 export default Home;

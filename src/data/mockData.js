d418fbee0894d8dc70c258c9abab1a79a5bf81aa
widrns15 <widrns15@naver.com>
2023-01-11 07:11:53 +0900

    design: 홈페이지 상품 설명 수정 및 줄바꿈

--- src/data/mockData.js
+++ src/data/mockData.js
@@ -4,7 +4,7 @@ export const mockTheme1Produdcts = [
     price: 21800,
     name: "비숑 블랙 머그잔",
     description:
-      "쌀쌀한 날씨에 따뜻한 우유, 커피 한잔하기 좋은 블랙 & 화이트 비숑 머그잔입니다.",
+      "쌀쌀한 날씨에 따뜻한 우유, 커피 한잔하기 좋은 \n 블랙 & 화이트 비숑 머그잔입니다.",
     thumbnail:
       "https://raw.githubusercontent.com/congchu/coment-shop-server/master/assets/images/product1.jpg",
     mainImage:
@@ -16,7 +16,7 @@ export const mockTheme1Produdcts = [
     price: 5000,
     name: "가열 보온 티코스터 온열 원터치 컵 받침대",
     description:
-      "언제나 따뜻하게 최대 :2시간 동안 최대 60도의 온도로 따듯한 차를 즐길 수 있습니다.",
+      "언제나 따뜻하게 최대 12시간 동안 \n 최대 60도의 온도로 따듯한 차를 즐길 수 있습니다.",
     thumbnail:
       "https://raw.githubusercontent.com/congchu/coment-shop-server/master/assets/images/product3.jpg",
 
@@ -28,7 +28,7 @@ export const mockTheme1Produdcts = [
     price: 5000,
     name: "벨루즈까사 솜사탕 파스텔 머그 4종 세트",
     description:
-      "솜사탕처럼 부드러: 쉐입에 스트라이프 조각이 더해져 심플하면서도 세련된 파스텔 컬러의 머그.",
+      "솜사탕처럼 부드러운 쉐입에 스트라이프 조각이 \n 더해져 심플하면서도 세련된 파스텔 컬러의 머그.",
     thumbnail:
       "https://raw.githubusercontent.com/congchu/coment-shop-server/master/assets/images/product2.jpg",
     mainImage:
@@ -62,7 +62,7 @@ export const mockTheme2Produdcts = [
     price: 6000,
     name: "얼음 트레이 얼음틀 36구 4p + 아이스큐브",
     description:
-      "여름에만 얼음을 이용하지 않는 것처럼, 이제는 냉동실에 한번 얼려서 사계절 내내 드세요",
+      "여름에만 얼음을 이용하지 않는 것처럼, \n 이제는 냉동실에 한번 얼려서 사계절 내내 드세요",
     thumbnail:
       "https://raw.githubusercontent.com/congchu/coment-shop-server/master/assets/images/product6.jpg",
     mainImage:
